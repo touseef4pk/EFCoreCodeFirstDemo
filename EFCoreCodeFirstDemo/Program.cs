ee4759a22f81e38126fccb3fa1906efc978a0b47
Touseef Khan <touseefkhan4pk@gmail.com>
2025-02-16 15:44:17 +0500

    ef core code first demo

---
+++ EFCoreCodeFirstDemo/Program.cs
@@ -0,0 +1,21 @@
+using EFCoreCodeFirstDemo.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
